73dac8a3b43daac9d0243314506b322179b1ef67
Tanya_Ak <tanyaomondi@gmail.com>
2020-05-05 17:59:23 +0200

    Created module for hash table ADT
    Created module for hash table ADT for implementing a look up table of unique key-value pairs

---
+++ src/hash_table.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/** Hash table implementation
+
+  @File Name
+    hash_table.c
+
+  @Summary
+    Function definitions for hash table ADT.
+
+  @Description
+    This implementation file contains the function definitions used in the 
+    implementation of a hash table ADT. This ADT is a table of structures with 
+    unique keys to data values. The key is of type int whereas the data is of 
+    type string. This implementation is adapted from the following URL:
+    https://www.tutorialspoint.com/data_structures_algorithms/hash_table_program_in_c.htm
+ */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: File Scope or Global Data                                         */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*  A brief description of a section can be given directly below the section
+    banner.
+ */
+
+/* ************************************************************************** */
+/** Descriptive Data Item Name
+
+  @Summary
+    Brief one-line summary of the data item.
+    
+  @Description
+    Full description, explaining the purpose and usage of data item.
+    <p>
+    Additional description in consecutive paragraphs separated by HTML 
+    paragraph breaks, as necessary.
+    <p>
+
+ */
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+// Section: Local Functions                                                   */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*  A brief description of a section can be given directly below the section
+    banner.
+ */
+
+/* ************************************************************************** */
+
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+// Section: Interface Functions                                               */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*  A brief description of a section can be given directly below the section
+    banner.
+ */
+
+
+
+/* *****************************************************************************
+ End of File
+ */
