bec7a26c668a67ac04511aef1d00128150a189d4
Tanya_Ak <tanyaomondi@gmail.com>
2020-05-06 08:51:18 +0200

    Added unit test file for morse_code.c

---
+++ test/test_morse_code.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+#include "morse_code.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_morse_code_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement morse_code");
+}
