8230059fd9176be4a0c29d2eae183a2a9183dbc5
Tanya_Ak <tanyaomondi@gmail.com>
2020-05-06 18:10:07 +0200

    Added unit test for encode function
    Added unit test for encode function to test morse code generation for the passed in string

--- test/test_morse_code.c
+++ test/test_morse_code.c
@@ -1,10 +1,9 @@
 #include "unity.h"
-
 #include "morse_code.h"
 #include "hash_table.h"
 
-data_item_t *hash_array[TABLE_SIZE];
-
+#define MAX_SIZE 80
+#define STRING_SIZE 8
 
 void setUp(void)
 {
@@ -21,8 +20,37 @@ void test_morse_code_NeedToImplement(void)
 
 void test_convert_to_uppercase(void)
 {
-    char word [8] = "taNya";
+    char word [STRING_SIZE] = "taNya";
 	convert_to_upper_case (word);
 	TEST_ASSERT_EQUAL_STRING("TANYA", word);
+}
 
+void test_encode(void)
+{
+	char morse_code[MAX_SIZE][STRING_SIZE];
+	char a[STRING_SIZE];
+	char four[STRING_SIZE];
+	char star[STRING_SIZE];
+	char space[STRING_SIZE];
+	char hyp[STRING_SIZE];
+	char enn[STRING_SIZE];
+	char nine[STRING_SIZE];
+	
+	encode("A4@ -N9",morse_code);
+	
+	strcpy(a,morse_code[0]);
+	strcpy(four,morse_code[1]);
+	strcpy(star,morse_code[2]);
+	strcpy(space,morse_code[3]);
+	strcpy(hyp,morse_code[4]);
+	strcpy(enn,morse_code[5]);
+	strcpy(nine,morse_code[6]);
+	
+	TEST_ASSERT_EQUAL_STRING(".-",a);
+	TEST_ASSERT_EQUAL_STRING("....-",four);
+	TEST_ASSERT_EQUAL_STRING(".--.-.",star);
+	TEST_ASSERT_EQUAL_STRING(" ",space);
+	TEST_ASSERT_EQUAL_STRING("-....-",hyp);
+	TEST_ASSERT_EQUAL_STRING("-.",enn);
+	TEST_ASSERT_EQUAL_STRING("----.",nine);
 }
