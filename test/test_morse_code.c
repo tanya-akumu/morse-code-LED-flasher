16e2bb20788d2303896efc3671fcea593d3850d0
Tanya_Ak <tanyaomondi@gmail.com>
2020-05-06 11:53:02 +0200

    added unit test for convert_to_uppercase function
    Added a unit test fucntion for convert to uppercase function
    
    Had to include the hash_table.h fail to access functions used in morse_code.c

--- test/test_morse_code.c
+++ test/test_morse_code.c
@@ -1,6 +1,10 @@
 #include "unity.h"
 
 #include "morse_code.h"
+#include "hash_table.h"
+
+data_item_t *hash_array[TABLE_SIZE];
+
 
 void setUp(void)
 {
@@ -13,4 +17,12 @@ void tearDown(void)
 void test_morse_code_NeedToImplement(void)
 {
     TEST_IGNORE_MESSAGE("Need to Implement morse_code");
+}
+
+void test_convert_to_uppercase(void)
+{
+    char word [8] = "taNya";
+	convert_to_upper_case (word);
+	TEST_ASSERT_EQUAL_STRING("TANYA", word);
+
 }
