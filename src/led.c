843586af11ab09ea82307d804d7dbf4714d69773
Tanya_Ak <tanyaomondi@gmail.com>
2020-05-06 22:47:11 +0200

    Added led module files
    Added led module files to "flash" according to the morse code character

---
+++ src/led.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/** led module
+
+  @File Name
+    led.c
+
+  @Summary
+    Module to "flash" led.
+
+  @Description
+    This module configures the different functions to "flash" an LED according
+    to the morse code character. A morse code character can be a dot, a dash, a
+    word space or a letter space. The "flashing" of the LED is implemented in a
+    way such that the delay time is different for each morse code character. */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: File Scope or Global Data                                         */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+
+/* ************************************************************************** */
+/** Descriptive Data Item Name
+
+  @Summary
+    Brief one-line summary of the data item.
+    
+  @Description
+    Full description, explaining the purpose and usage of data item.
+    <p>
+    Additional description in consecutive paragraphs separated by HTML 
+    paragraph breaks, as necessary.
+    <p>
+    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+    
+  @Remarks
+    Any additional remarks
+ */
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+// Section: Local Functions                                                   */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*  A brief description of a section can be given directly below the section
+    banner.
+ */
+
+/* ************************************************************************** */
+
+/** 
+  @Function
+    int ExampleLocalFunctionName ( int param1, int param2 ) 
+
+  @Summary
+    Brief one-line description of the function.
+
+  @Description
+    Full description, explaining the purpose and usage of the function.
+    <p>
+    Additional description in consecutive paragraphs separated by HTML 
+    paragraph breaks, as necessary.
+    <p>
+    Type "JavaDoc" in the "How Do I?" IDE toolbar for more information on tags.
+
+  @Precondition
+    List and describe any required preconditions. If there are no preconditions,
+    enter "None."
+
+  @Parameters
+    @param param1 Describe the first parameter to the function.
+    
+    @param param2 Describe the second parameter to the function.
+
+  @Returns
+    List (if feasible) and describe the return values of the function.
+    <ul>
+      <li>1   Indicates an error occurred
+      <li>0   Indicates an error did not occur
+    </ul>
+
+  @Remarks
+    Describe any special behavior not described above.
+    <p>
+    Any additional remarks.
+
+ */
+
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+// Section: Interface Functions                                               */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*  A brief description of a section can be given directly below the section
+    banner.
+ */
+
+// *****************************************************************************
+
+/** 
+  @Function
+    int ExampleInterfaceFunctionName ( int param1, int param2 ) 
+
+  @Summary
+    Brief one-line description of the function.
+
+  @Remarks
+    Refer to the example_file.h interface header for function usage details.
+ */
+
+
+
+/* *****************************************************************************
+ End of File
+ */
---
+++ src/led.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/** led module
+
+  @File Name
+    led.h
+
+  @Summary
+    Module to "flash" led.
+
+  @Description
+    This module configures the different functions to "flash" an LED according
+    to the morse code character. A morse code character can be a dot, a dash, a
+    word space or a letter space. The "flashing" of the LED is implemented in a
+    way such that the delay time is different for each morse code character.
+ */
+/* ************************************************************************** */
+
+
+
+/* *****************************************************************************
+ End of File
+ */
